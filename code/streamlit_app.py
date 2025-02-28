2931b5f5c8070d2ffe7f0b7a9473fc1513ea6e5a
CV0601 <107110689+CV0601@users.noreply.github.com>
2025-02-28 16:18:00 +0000

    layout van website aangepast naar 2x2

--- code/streamlit_app.py
+++ code/streamlit_app.py
@@ -1,14 +1,17 @@
 import streamlit as st
 import pandas as pd
 import math
+import datetime as dt
 from pathlib import Path
 
 logo_url = 'https://einderinvestments.nl/wp-content/uploads/2024/09/Verticaal-Wit.png'
-
+date_today = dt.date.today()
+formatted_date_today = date_today.strftime('%d-%m-%Y')
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
     page_title='Einder Investments Portfolio Dashboard',
     page_icon= logo_url, # This is an emoji shortcode. Could be a URL too.
+    layout="wide"
 )
 
 # -----------------------------------------------------------------------------
@@ -101,82 +104,93 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
-
+# creating 4 columns (quadrants)
+col1, col2 = st.columns(2)
+with col1: 
+    st.subheader('Returns', divider = 'blue')
+    selected_return = st.multiselect(
+    'What returns would you like to view?',
+    ['Daily', 'Monthly', 'Quarterly', 'Annual'])
+    st.write('Content here')
 # Add some spacing
 ''
 ''
+with col2:
+    st.subheader('Portfolio Value over time', divider='blue')
+    min_value = gdp_df['Year'].min()
+    max_value = gdp_df['Year'].max()
 
-min_value = gdp_df['Year'].min()
-max_value = gdp_df['Year'].max()
+    from_year, to_year = st.slider(
+        'Which years are you interested in?',
+        min_value=min_value,
+        max_value=max_value,
+        value=[min_value, max_value])
 
-from_year, to_year = st.slider(
-    'Which years are you interested in?',
-    min_value=min_value,
-    max_value=max_value,
-    value=[min_value, max_value])
+    countries = gdp_df['Country Code'].unique()
 
-countries = gdp_df['Country Code'].unique()
+    if not len(countries):
+        st.warning("Select at least one country")
 
-if not len(countries):
-    st.warning("Select at least one country")
+    selected_countries = st.multiselect(
+        'Which countries would you like to view?',
+        countries,
+        ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
 
-selected_countries = st.multiselect(
-    'Which countries would you like to view?',
-    countries,
-    ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
+    ''
+    ''
+    ''
 
-''
-''
-''
+    # Filter the data
+    filtered_gdp_df = gdp_df[
+        (gdp_df['Country Code'].isin(selected_countries))
+        & (gdp_df['Year'] <= to_year)
+        & (from_year <= gdp_df['Year'])
+    ]
 
-# Filter the data
-filtered_gdp_df = gdp_df[
-    (gdp_df['Country Code'].isin(selected_countries))
-    & (gdp_df['Year'] <= to_year)
-    & (from_year <= gdp_df['Year'])
-]
+ 
 
-st.header('GDP over time', divider='gray')
+    ''
 
-''
-
-st.line_chart(
-    filtered_gdp_df,
-    x='Year',
-    y='GDP',
-    color='Country Code',
-)
-
-''
-''
-
-
-first_year = gdp_df[gdp_df['Year'] == from_year]
-last_year = gdp_df[gdp_df['Year'] == to_year]
-
-st.header(f'GDP in {to_year}', divider='gray')
+    st.line_chart(
+        filtered_gdp_df,
+        x='Year',
+        y='GDP',
+        color='Country Code',
+    )
 
 ''
+''
+col3, col4 = st.columns(2)
+with col3:
+    first_year = gdp_df[gdp_df['Year'] == from_year]
+    last_year = gdp_df[gdp_df['Year'] == to_year]
 
-cols = st.columns(4)
+    st.subheader(f'Performance in {to_year}', divider='blue')
 
-for i, country in enumerate(selected_countries):
-    col = cols[i % len(cols)]
+    ''
 
-    with col:
-        first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-        last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
+    cols = st.columns(4)
 
-        if math.isnan(first_gdp):
-            growth = 'n/a'
-            delta_color = 'off'
-        else:
-            growth = f'{last_gdp / first_gdp:,.2f}x'
-            delta_color = 'normal'
+    for i, country in enumerate(selected_countries):
+        col = cols[i % len(cols)]
 
-        st.metric(
-            label=f'{country} GDP',
-            value=f'{last_gdp:,.0f}B',
-            delta=growth,
-            delta_color=delta_color
-        )
+        with col:
+            first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
+            last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
+
+            if math.isnan(first_gdp):
+                growth = 'n/a'
+                delta_color = 'off'
+            else:
+                growth = f'{last_gdp / first_gdp:,.2f}x'
+                delta_color = 'normal'
+
+            st.metric(
+                label=f'{country} GDP',
+                value=f'{last_gdp:,.0f}B',
+                delta=growth,
+                delta_color=delta_color
+            )
+with col4:
+    st.subheader(f'Holdings as of {formatted_date_today}', divider='blue')
+    st.table()
