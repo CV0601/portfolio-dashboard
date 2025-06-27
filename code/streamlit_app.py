cef3ae5352a4b60efe171b5699aa4ca4d152711e
Casper <casper.de.vries@ziggo.nl>
2025-06-27 15:47:29 +0200

    included new data

--- code/streamlit_app.py
+++ code/streamlit_app.py
@@ -19,7 +19,7 @@ st.set_page_config(
 # Declare some useful functions.
 
 @st.cache_data
-def get_gdp_data():
+def get_data():
     """Grab GDP data from a CSV file.
 
     This uses caching to avoid having to read the file every time. If we were
@@ -28,43 +28,30 @@ def get_gdp_data():
     """
 
     # Instead of a CSV on disk, you could read from an HTTP endpoint here too.
-    DATA_FILENAME = Path(__file__).parent/'data/gdp_data.csv'
-    raw_gdp_df = pd.read_csv(DATA_FILENAME)
+    DATA_FILENAME = Path(__file__).parent/'data/data_test_24052025.csv'
+    raw_df = pd.read_csv(DATA_FILENAME, header = None)
+    first_col = raw_df.columns[0]
+    # Create a dict of DataFrames, one for each unique value
+    df_dict = {
+        key: raw_df[raw_df[first_col] == key].copy()
+        for key in raw_df[first_col].unique()
+    }
+    # Available keys:
+    # ['Introduction', 'Profile', 
+    # 'Key Statistics', 'Open Position Summary', 
+    # 'Time Period Benchmark Comparison', 
+    # 'Risk Measures Benchmark Comparison', 'Disclosure']
 
-    MIN_YEAR = 1960
-    MAX_YEAR = 2022
+    return df_dict
 
-    # The data above has columns like:
-    # - Country Name
-    # - Country Code
-    # - [Stuff I don't care about]
-    # - GDP for 1960
-    # - GDP for 1961
-    # - GDP for 1962
-    # - ...
-    # - GDP for 2022
-    #
-    # ...but I want this instead:
-    # - Country Name
-    # - Country Code
-    # - Year
-    # - GDP
-    #
-    # So let's pivot all those year-columns into two: Year and GDP
-    gdp_df = raw_gdp_df.melt(
-        ['Country Code'],
-        [str(x) for x in range(MIN_YEAR, MAX_YEAR + 1)],
-        'Year',
-        'GDP',
-    )
-
-    # Convert years from string to integers
-    gdp_df['Year'] = pd.to_numeric(gdp_df['Year'])
-
-    return gdp_df
-
-gdp_df = get_gdp_data()
-
+df = get_data()
+def clean_for_streamlit(df):
+    df = df.copy()
+    df.columns = df.columns.map(str)
+    df = df.astype(object).where(pd.notnull(df), None)
+    for col in df.select_dtypes(include=['int64', 'float64']).columns:
+        df[col] = df[col].apply(lambda x: int(x) if pd.notnull(x) else None)
+    return df
 # -----------------------------------------------------------------------------
 # Draw the actual page
 
@@ -112,86 +99,110 @@ with col1:
     selected_return = st.multiselect(
     'What returns would you like to view?',
     ['Daily', 'Monthly', 'Quarterly', 'Annual'])
+    df['Time Period Benchmark Comparison'].columns = df['Time Period Benchmark Comparison'].iloc[1]
+    df['Time Period Benchmark Comparison'] = df['Time Period Benchmark Comparison'][2:]
+    df['Time Period Benchmark Comparison']['Date'] = pd.to_datetime(df['Time Period Benchmark Comparison']['Date'], errors = 'coerce')
     st.write('Content here')
 # Add some spacing
 ''
 ''
 with col2:
-    st.subheader('Portfolio Value over time', divider='blue')
-    min_value = gdp_df['Year'].min()
-    max_value = gdp_df['Year'].max()
+    st.subheader('Portfolio Returns over time', divider='blue')
+    # min_value = df['Year'].min()
+    # max_value = df['Year'].max()
 
-    from_year, to_year = st.slider(
-        'Which years are you interested in?',
-        min_value=min_value,
-        max_value=max_value,
-        value=[min_value, max_value])
+    # from_year, to_year = st.slider(
+    #     'Which years are you interested in?',
+    #     min_value=min_value,
+    #     max_value=max_value,
+    #     value=[min_value, max_value])
 
-    countries = gdp_df['Country Code'].unique()
+    # countries = df['Country Code'].unique()
 
-    if not len(countries):
-        st.warning("Select at least one country")
+    # if not len(countries):
+    #     st.warning("Select at least one country")
 
-    selected_countries = st.multiselect(
-        'Which countries would you like to view?',
-        countries,
-        ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
+    # selected_countries = st.multiselect(
+    #     'Which countries would you like to view?',
+    #     countries,
+    #     ['DEU', 'FRA', 'GBR', 'BRA', 'MEX', 'JPN'])
 
-    ''
-    ''
-    ''
+    # ''
+    # ''
+    # ''
 
-    # Filter the data
-    filtered_gdp_df = gdp_df[
-        (gdp_df['Country Code'].isin(selected_countries))
-        & (gdp_df['Year'] <= to_year)
-        & (from_year <= gdp_df['Year'])
-    ]
+    # # Filter the data
+    # filtered_df = df[
+    #     (df['Country Code'].isin(selected_countries))
+    #     & (df['Year'] <= to_year)
+    #     & (from_year <= df['Year'])
+    # ]
 
  
 
-    ''
+    # ''
 
-    st.line_chart(
-        filtered_gdp_df,
-        x='Year',
-        y='GDP',
-        color='Country Code',
-    )
+    # st.line_chart(
+    #     filtered_df,
+    #     x='Year',
+    #     y='GDP',
+    #     color='Country Code',
+    # )
 
 ''
 ''
 col3, col4 = st.columns(2)
 with col3:
-    first_year = gdp_df[gdp_df['Year'] == from_year]
-    last_year = gdp_df[gdp_df['Year'] == to_year]
+    # first_year = df[df['Year'] == from_year]
+    # last_year = df[df['Year'] == to_year]
 
-    st.subheader(f'Performance in {to_year}', divider='blue')
+    # st.subheader(f'Performance in {to_year}', divider='blue')
 
     ''
 
-    cols = st.columns(4)
+    # cols = st.columns(4)
 
-    for i, country in enumerate(selected_countries):
-        col = cols[i % len(cols)]
+    # for i, country in enumerate(selected_countries):
+    #     col = cols[i % len(cols)]
 
-        with col:
-            first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
-            last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
+    #     with col:
+    #         first_gdp = first_year[first_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
+    #         last_gdp = last_year[last_year['Country Code'] == country]['GDP'].iat[0] / 1000000000
 
-            if math.isnan(first_gdp):
-                growth = 'n/a'
-                delta_color = 'off'
-            else:
-                growth = f'{last_gdp / first_gdp:,.2f}x'
-                delta_color = 'normal'
+    #         if math.isnan(first_gdp):
+    #             growth = 'n/a'
+    #             delta_color = 'off'
+    #         else:
+    #             growth = f'{last_gdp / first_gdp:,.2f}x'
+    #             delta_color = 'normal'
 
-            st.metric(
-                label=f'{country} GDP',
-                value=f'{last_gdp:,.0f}B',
-                delta=growth,
-                delta_color=delta_color
-            )
+    #         st.metric(
+    #             label=f'{country} GDP',
+    #             value=f'{last_gdp:,.0f}B',
+    #             delta=growth,
+    #             delta_color=delta_color
+    #         )
 with col4:
+    # 1. Set headers
+    df['Open Position Summary'].columns = df['Open Position Summary'].iloc[1]
+
+    # 2. Remove the header rows
+    df['Open Position Summary'] = df['Open Position Summary'].iloc[2:].copy()
+
+    # 3. Convert to datetime
+    df['Open Position Summary']['Date'] = pd.to_datetime(df['Open Position Summary']['Date'], errors='coerce')
+
+    # 4. Drop rows where Symbol is NaN
+    df['Open Position Summary'].dropna(subset=["Symbol"], inplace=True)
+
+    # 5. Ensure 'Sector' column exists before dropping
+    if "Sector" in df['Open Position Summary'].columns:
+        df['Open Position Summary'].drop(columns=["Sector"], inplace=True)
+
+    # 6. Slice columns (if you're sure you want cols 5 to 12 *after* Sector is dropped)
+    df_unclean = df['Open Position Summary'].iloc[:, 5:12]
+    df_unclean = df_unclean.fillna('N/A')
+    df_display = clean_for_streamlit(df_unclean)
+    
     st.subheader(f'Holdings as of {formatted_date_today}', divider='blue')
-    st.table()
+    st.table(df_display)
